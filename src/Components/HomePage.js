72d8bf968f490f8fa19fb176b5f900f62c069262
Logith-lightcast <logith.r@emsibg.com>
2023-12-27 17:12:08 +0530

    final

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import './HomePage.css'; 
+import Logo from '../Images/Logo.jpg';
+import Home from '../Images/Home-Left.avif'
+
 
 const HomePage = () => {
     return (
       <div>
         <header>
-          <h1>Demo</h1>
+        <img src={Logo} alt="Description of the image" style={{ width: '235px', height: '38px' , marginLeft:'140px', marginTop:'20px'}}  />
           <nav>
             <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
+              <li>SOLUTIONS</li>
+              <li>ABOUT</li>
+              <li>RESOURCES</li>
+              <li>PRICING</li>
             </ul>
+            <ol  className='left'>
+            <li>Log in</li>
+              <li >Contact</li>
+              <li>US</li>
+            </ol>
           </nav>
         </header>
   
         <main>
-          <section>
-            <h2>Welcome to Our Home Page</h2>
-            <p>This is the main content of the home page.</p>
-          </section>
+          <img src={Home} alt='Front page'  style={{width:'50%' ,float:'right', marginTop:'-14%'}}/>
+          <h1 style={{fontSize:'50px', marginTop:'245px', marginLeft:'-70px'}}>A Global Leader in <br className='left1'/>Labout Market <br className='left2'/> Analytics</h1>
+          <button style={{padding:'17px', backgroundColor:'rgb(245 69 98)', color:'white', fontSize:'20px', borderStyle:"none", marginLeft:'260px'}} >Contact Us Now </button>
         </main>
   
         <footer>
